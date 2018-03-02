b2738fe88919ed4abd8cbd273d7d32c1143b34ec
Andrew Malone <admmorketh@gmail.com>
2018-03-02 02:18:31 -0700

    Begins work for Automated Assemblers

--- Automated Assmblers/Program.cs
+++ Automated Assmblers/Program.cs
@@ -18,22 +18,7 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-
+        string AutomatedAssemblerTAG = "[AI]";
         public Program()
         {
             // The constructor, called only once every session and
@@ -54,13 +39,22 @@ namespace IngameScript
             // needed.
         }
 
-        public void Main(string argument)
+        public void Main()
         {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked.
-            // 
-            // The method itself is required, but the argument above
-            // can be removed if not needed.
+            List<IMyAssembler> blocks = new List<IMyAssembler>();
+            GridTerminalSystem.GetBlocksOfType(blocks);
+
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                Echo("Total Blocks Found: "+blocks.Count.ToString());
+                Echo("Is AI: "+blocks[i].CustomName.StartsWith(AutomatedAssemblerTAG));
+                Echo("Full Name: " + blocks[i].CustomName);
+
+                if (blocks[i].CustomName.StartsWith(AutomatedAssemblerTAG))
+                {
+                    // Get INI Config from Assembler Custom Data
+                }
+            }
         }
     }
 }
--- Automated Assmblers/Readme.cs
+++ Automated Assmblers/Readme.cs
@@ -1,8 +1,3 @@
 ﻿/*
- *   R e a d m e
- *   -----------
- * 
- *   In this file you can include any instructions or other comments you want to have injected onto the 
- *   top of your final script. You can safely delete this file if you do not want any such comments.
- * 
+ *  Collective Industries bring you the latest in Automated Assemblers in order to streamline your production que. 
  */
